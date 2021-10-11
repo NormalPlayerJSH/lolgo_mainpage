9f0c57a3bf3dbefadc9d90ebabcda65c98b64551
NormalPlayerJSH <jsh010904@gmail.com>
2021-10-11 19:28:07 +0900

    밴픽툴 링크 추가

--- src/Pages/Header/Header.tsx
+++ src/Pages/Header/Header.tsx
@@ -7,6 +7,16 @@ export default function Header() {
     <div id={styles.headerDiv}>
       <div id={styles.headerInner}>
         <img src={logoInline} alt="" id={styles.headerLogo} onClick={()=>{window.location.href='/'}}/>
+        <div className={styles.navLinks}>
+          <div
+            className={`${styles.navLink} ${styles.navText}`}
+            onClick={() => {
+              window.location.href='https://banpick.lolgo.gg';
+            }}
+          >
+            Banpick Tool
+          </div>
+        </div>
       </div>
     </div>
   );
