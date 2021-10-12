dace0d7e4b838a76c405b46a0adff66a37b7c3a4
NormalPlayerJSH <jsh010904@gmail.com>
2021-10-13 00:13:18 +0900

    https 설정

--- src/Meta/DataDragon/index.ts
+++ src/Meta/DataDragon/index.ts
@@ -5,20 +5,20 @@ import { PerksMeta } from '../PerksMeta';
 import { PerksImage } from './PerksImage';
 import EmptyImage from '../empty.png';
 
-export const getProfileIcon = (profileIconId:number) => `http://ddragon.leagueoflegends.com/cdn/${currentVersion}/img/profileicon/${profileIconId}.png`;
+export const getProfileIcon = (profileIconId:number) => `https://ddragon.leagueoflegends.com/cdn/${currentVersion}/img/profileicon/${profileIconId}.png`;
 
 export const getChampionImage = (key:number) => {
-  if (ChampMeta[key].id && ChampMeta[key].id !== 'None') return `http://ddragon.leagueoflegends.com/cdn/${currentVersion}/img/champion/${ChampMeta[key].id}.png`;
+  if (ChampMeta[key].id && ChampMeta[key].id !== 'None') return `https://ddragon.leagueoflegends.com/cdn/${currentVersion}/img/champion/${ChampMeta[key].id}.png`;
   return EmptyImage;
 };
 
-export const getSpellImage = (key:number) => `http://ddragon.leagueoflegends.com/cdn/${currentVersion}/img/spell/${SpellMeta[key].id}.png`;
+export const getSpellImage = (key:number) => `https://ddragon.leagueoflegends.com/cdn/${currentVersion}/img/spell/${SpellMeta[key].id}.png`;
 
 export const getPerksImage = (id:number) => PerksImage[PerksMeta[id].key];
 
 export const getItemImage = (itemId:number) => {
   if (itemId === 0) return EmptyImage;
-  return `http://ddragon.leagueoflegends.com/cdn/${currentVersion}/img/item/${itemId}.png`;
+  return `https://ddragon.leagueoflegends.com/cdn/${currentVersion}/img/item/${itemId}.png`;
 };
 
 export default {
