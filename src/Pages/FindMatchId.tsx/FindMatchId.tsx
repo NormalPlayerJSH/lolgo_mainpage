121c620bb9da84bd88d2e03262e2096c3842817a
NormalPlayerJSH <jsh010904@gmail.com>
2021-10-18 20:22:00 +0900

    최근 검색 소환사명 삭제 기능 추가

--- src/Pages/FindMatchId.tsx/FindMatchId.tsx
+++ src/Pages/FindMatchId.tsx/FindMatchId.tsx
@@ -8,13 +8,27 @@ import { storageKey } from '../../types/enum';
 
 function FindMatchId() {
   const [recentNick, setRecentNick] = useState<string[]>([]);
-  useEffect(() => {
+  const getRecentNick = () => {
     const recent = window.localStorage.getItem(storageKey.lolgoNickname);
     if(recent){
       const recentList:string[] = JSON.parse(recent);
       setRecentNick(recentList);
-      console.log(recentList)
+    } else setRecentNick([]);
+  }
+  const deleteRecentNick = (nickname:string) => {
+    const recent = window.localStorage.getItem(storageKey.lolgoNickname);
+    if(recent) {
+      let recentList:string[] = JSON.parse(recent);
+      const nickIndex = recentList.indexOf(nickname);
+      if(nickIndex!==-1) {
+        recentList = recentList.slice(0,nickIndex).concat(recentList.slice(nickIndex+1));
+        window.localStorage.setItem(storageKey.lolgoNickname,JSON.stringify(recentList));
+      }
     }
+    getRecentNick();
+  }
+  useEffect(() => {
+    getRecentNick();
     (document.getElementById('mainWideAd') as Element).innerHTML=`
     <ins class="kakao_ad_area" style="display:none;" 
     data-ad-unit    = "DAN-Idda7Hj8DNO1VZ9S" 
@@ -64,7 +78,10 @@ function FindMatchId() {
             <div className={styles.recommendListDiv}>
               {
                 recentNick.map((gameNum)=>(
+                  <div className={styles.recentDiv}>
                     <a href={`/findmatchid/${gameNum}`} className={styles.recommendLink}>{gameNum}</a>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" onClick={()=>{deleteRecentNick(gameNum)}}><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+                  </div>
                   ))
               }
             </div></>:
